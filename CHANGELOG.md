de3526f777b5f65faf185f492de6230ed398aaaa
alli959 <alexandergudmundsson@gmail.com>
2024-03-04 19:00:06 +0000

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,76 @@
+## 0.6.14
+
+- chore: Amplify Swift version bump to 1.30.4 (#3328)
+
+## 0.6.13 (2023-03-22)
+
+- chore: Bump Amplify iOS to 1.29.1
+
+## 0.6.12 (2023-01-24)
+
+## 0.6.11 (2023-01-12)
+
+## 0.6.10 (2022-11-09)
+
+- chore: Bump iOS dependency to 1.28.2 ([#2334](https://github.com/aws-amplify/amplify-flutter/pull/2334))
+
+## 0.6.9 (2022-10-12)
+
+## 0.6.8 (2022-09-19)
+
+## 0.6.7 (2022-09-07)
+
+## 0.6.6 (2022-08-16)
+
+- chore: bump amplify-ios dep to 1.28.0
+
+## 0.6.5 (2022-07-28)
+
+## 0.6.4 (2022-07-25)
+
+## 0.6.3 (2022-07-21)
+
+## 0.6.2 (2022-07-14)
+
+- feat(auth): runtime authFlow attribute ([#1687](https://github.com/aws-amplify/amplify-flutter/pull/1687))
+
+## 0.6.1 (2022-06-30)
+
+## 0.6.0 (2022-06-20)
+
+- Bump minimum Dart SDK to 2.15
+
+## 0.5.1 (2022-05-24)
+
+- Update pub.dev links
+
+## 0.5.0 (2022-05-17)
+
+### Chores
+
+- chore: make example Android Apps runnable with API 32+ (#1474)
+- chore(auth): Templatize and update exception comments (#1476)
+- chore: update android compileSdkVersion to 31
+- chore: upgrade gradle plugin to 7.1.2
+- chore: enable android codebase linter checks
+- chore: replace 0.4.2-1 with 0.4.3 due to melos limitation (#1496)
+- chore: Lint fixes (#1471)
+- chore: Flutter 3 fixes (#1580)
+
+## 0.4.5 (2022-04-13)
+
+-fix: bumps ios version and bumps api AuthProvider timeout (#1526)
+
+## 0.4.4 (2022-04-06)
+
+## 0.4.3 (2022-04-02)
+
+- chore: bump amplify-ios to 1.22.3
+
+## 0.4.2 (2022-03-24)
+
+- chore: bump amplify-ios to 1.22.0 (#1468)
+
+## 0.4.1
+
+- Initial release
---
+++ ios/Classes/FlutterSignInRequest.swift
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import Foundation
+import Amplify
+import AmplifyPlugins
+import AWSCore
+import amplify_core
+
+struct FlutterSignInRequest {
+  var username: String?
+  var password: String?
+  var options: AuthSignInOperation.Request.Options?
+  init(dict: NSMutableDictionary){
+    self.username = dict["username"] as? String
+    self.password = dict["password"] as? String
+    self.options = formatOptions(options: dict["options"] as! Dictionary<String, Any>?)
+  }
+    
+   func formatOptions(options: Dictionary<String, Any>?) -> AuthSignInOperation.Request.Options {
+     
+     let authFlowType: AuthFlowType = {
+       switch options?["authFlowType"] as? String {
+         case "userSrpAuth":
+           return AuthFlowType.userSRP
+         case "customAuth":
+           return AuthFlowType.custom
+         default:
+           return AuthFlowType.unknown
+         }
+     }()
+    
+     let pluginOptions = AWSAuthSignInOptions(
+       metadata: options?["clientMetadata"] as? [String : String],
+       authFlowType: authFlowType
+     )
+       
+     return AuthSignInOperation.Request.Options(pluginOptions: pluginOptions)
+  }}
